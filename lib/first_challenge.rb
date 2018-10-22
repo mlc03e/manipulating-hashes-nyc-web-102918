4396ffca3f0511c64d5080b84f4a95aa46f651a9
Meghan Campbell <mlc03e@gmail.com>
2018-10-22 22:20:29 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,9 @@ def first_challenge
   }
 
   #your code here
-
-
+ if contacts["Freddy Mercury"][:favorite_icecream_flavors] == "strawberry"
+   delete "strawberry"
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
